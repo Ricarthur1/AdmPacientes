7216c037c983e0a8a2d3c1ba1f4d4835cd4e408a
Ricardo Alexis Lopez Arthur <19170613@itculiacan.edu.mx>
2023-11-25 13:40:44 -0700

    Generando ID para cada paciente agregado

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
 import {ref, reactive} from 'vue'
+import { uid } from 'uid'
+
 import Header from './components/Header.vue'
 import Formulario from './components/Formulario.vue'
 import Paciente from './components/Paciente.vue'
@@ -7,6 +9,7 @@ import Paciente from './components/Paciente.vue'
     const pacientes = ref([])
 
     const paciente = reactive({
+        id: null,
         nombre: '',
         propietario: '',
         email: '',
@@ -15,7 +18,7 @@ import Paciente from './components/Paciente.vue'
     })
 
     const guardarPaciente = () => {
-        pacientes.value.push({...paciente})
+        pacientes.value.push({...paciente, id: uid()})
 
         //Reiniciar el objeto
         Object.assign(paciente, {
