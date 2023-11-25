256cc8c40b64737c4db0df49e779bf5e5f606f84
Ricardo Alexis Lopez Arthur <19170613@itculiacan.edu.mx>
2023-11-25 13:24:13 -0700

    Mostrando los pacientes agregados

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import {ref, reactive} from 'vue'
 import Header from './components/Header.vue'
 import Formulario from './components/Formulario.vue'
+import Paciente from './components/Paciente.vue'
 
     const pacientes = ref([])
 
@@ -41,7 +42,15 @@ import Formulario from './components/Formulario.vue'
                 </h3>
 
                 <div v-if="pacientes.length > 0">
+                    <p class="text-lg mt-5 text-center mb-10">
+                        Información de
+                        <span class="text-indigo-600 font-bold">Pacientes</span>
+                    </p>
+                    <Paciente
+                        v-for="paciente in pacientes"
+                        :paciente="paciente"
 
+                    />
                 </div>
                 <p v-else class="mt-20 text-2xl text-center" >No hay Pacientes</p>
             </div>
